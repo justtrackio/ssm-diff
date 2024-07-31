9e28f715638fea1eea6d997e3f67b9e250d2ece8
Marco Frömbgen <23717573+mfroembgen@users.noreply.github.com>
2024-07-31 16:35:51 +0200

    feat: add backup command and refactor

--- setup.py
+++ setup.py
@@ -1,31 +1,45 @@
 #!/usr/bin/env python
 
 from os import path
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
-wd = path.abspath(path.dirname(__file__))
-with open(path.join(wd, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+def read_long_description():
+    wd = path.abspath(path.dirname(__file__))
+    with open(path.join(wd, 'README.md'), encoding='utf-8') as f:
+        return f.read()
+
 
 setup(
-    description = 'A tool to manage contents of AWS SSM Parameter Store',
-    name = 'ssm-diff',
-    version = '0.5',
-    author = 'Sergey Motovilovets',
-    author_email = 'motovilovets.sergey@gmail.com',
-    license='MIT',
-    url = 'https://github.com/runtheops/ssm-diff',
-    download_url = 'https://github.com/runtheops/ssm-diff/archive/0.5.tar.gz',
-    long_description=long_description,
+    name='ssm-diff',
+    version='0.6',
+    description='A tool to manage contents of AWS SSM Parameter Store',
+    long_description=read_long_description(),
     long_description_content_type='text/markdown',
-    keywords = ['aws', 'ssm', 'parameter-store'],
-    packages = ['states'],
+    url='https://github.com/justtrackio/ssm-diff',
+    download_url='https://github.com/justtrackio/ssm-diff/archive/0.6.tar.gz',
+    license='MIT',
+    packages=find_packages(),
     scripts=['ssm-diff'],
     install_requires=[
         'termcolor',
         'boto3',
         'dpath',
         'PyYAML'
-    ]
+    ],
+    keywords=['aws', 'ssm', 'parameter-store'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+    python_requires='>=3.6',
 )
--- states/helpers.py
+++ states/helpers.py
@@ -1,18 +1,15 @@
 from termcolor import colored
 from copy import deepcopy
 import collections
+from datetime import datetime
 
 
-class FlatDictDiffer(object):
+class FlatDictDiffer:
     def __init__(self, ref, target):
         self.ref, self.target = ref, target
         self.ref_set, self.target_set = set(ref.keys()), set(target.keys())
         self.isect = self.ref_set.intersection(self.target_set)
-
-        if self.added() or self.removed() or self.changed():
-            self.differ = True
-        else:
-            self.differ = False
+        self.differ = bool(self.added() or self.removed() or self.changed())
 
     def added(self):
         return self.target_set - self.isect
@@ -21,73 +18,65 @@ class FlatDictDiffer(object):
         return self.ref_set - self.isect
 
     def changed(self):
-        return set(k for k in self.isect if self.ref[k] != self.target[k])
+        return {k for k in self.isect if self.ref[k] != self.target[k]}
 
     def unchanged(self):
-        return set(k for k in self.isect if self.ref[k] == self.target[k])
+        return {k for k in self.isect if self.ref[k] == self.target[k]}
 
     def print_state(self):
         for k in self.added():
-            print(colored("+", 'green'), "{} = {}".format(k, self.target[k]))
+            print(colored("+", 'green'), f"{k} = {self.target[k]}")
 
         for k in self.removed():
             print(colored("-", 'red'), k)
 
         for k in self.changed():
-            print(colored("~", 'yellow'), "{}:\n\t< {}\n\t> {}".format(k, self.ref[k], self.target[k]))
+            print(colored("~", 'yellow'), f"{k}:\n\t< {self.ref[k]}\n\t> {self.target[k]}")
 
 
 def flatten(d, pkey='', sep='/'):
     items = []
-    for k in d:
-        new = pkey + sep + k if pkey else k
-        if isinstance(d[k], collections.MutableMapping):
-            items.extend(flatten(d[k], new, sep=sep).items())
+    for k, v in d.items():
+        new = f"{pkey}{sep}{k}" if pkey else k
+        if isinstance(v, collections.abc.MutableMapping):
+            items.extend(flatten(v, new, sep=sep).items())
         else:
-            items.append((sep + new, d[k]))
+            items.append((f"{sep}{new}", v))
     return dict(items)
 
 
 def add(obj, path, value):
     parts = path.strip("/").split("/")
-    last = len(parts) - 1
-    for index, part in enumerate(parts):
-        if index == last:
-            obj[part] = value
-        else:
-            obj = obj.setdefault(part, {})
+    for part in parts[:-1]:
+        obj = obj.setdefault(part, {})
+    obj[parts[-1]] = value
 
 
 def search(state, path):
     result = state
     for p in path.strip("/").split("/"):
-        if result.get(p):
-            result = result[p]
-        else:
-            result = {}
+        result = result.get(p, {})
+        if not result:
             break
-    output = {}
-    add(output, path, result)
-    return output
+    return {path: result}
 
 
 def unflatten(d):
     output = {}
-    for k in d:
-        add(
-            obj=output,
-            path=k,
-            value=d[k])
+    for k, v in d.items():
+        add(output, k, v)
     return output
 
 
 def merge(a, b):
-    if not isinstance(b, dict):
-        return b
     result = deepcopy(a)
-    for k in b:
-        if k in result and isinstance(result[k], dict):
-            result[k] = merge(result[k], b[k])
+    for k, v in b.items():
+        if isinstance(v, dict) and k in result and isinstance(result[k], dict):
+            result[k] = merge(result[k], v)
         else:
-            result[k] = deepcopy(b[k])
+            result[k] = deepcopy(v)
     return result
+
+
+def timestamp():
+    return datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
