520635d39514b6366f522d4b6e642bab535ce99e
pehlicd <65170388+pehlicd@users.noreply.github.com>
2024-08-06 11:13:13 +0200

    chore: added release workflow

--- setup.py
+++ setup.py
@@ -6,40 +6,35 @@ from setuptools import setup, find_packages
 
 def read_long_description():
     wd = path.abspath(path.dirname(__file__))
-    with open(path.join(wd, 'README.md'), encoding='utf-8') as f:
+    with open(path.join(wd, "README.md"), encoding="utf-8") as f:
         return f.read()
 
 
 setup(
-    name='ssm-diff',
-    version='0.6',
-    description='A tool to manage contents of AWS SSM Parameter Store',
+    name="ssm-diff",
+    version="v",
+    description="A tool to manage contents of AWS SSM Parameter Store",
     long_description=read_long_description(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/justtrackio/ssm-diff',
-    download_url='https://github.com/justtrackio/ssm-diff/archive/0.6.tar.gz',
-    license='MIT',
+    long_description_content_type="text/markdown",
+    url="https://github.com/justtrackio/ssm-diff",
+    download_url="https://github.com/justtrackio/ssm-diff/archive/v",
+    license="MIT",
     packages=find_packages(),
-    scripts=['ssm-diff'],
-    install_requires=[
-        'termcolor',
-        'boto3',
-        'dpath',
-        'PyYAML'
-    ],
-    keywords=['aws', 'ssm', 'parameter-store'],
+    scripts=["ssm-diff"],
+    install_requires=["termcolor", "boto3", "dpath", "PyYAML"],
+    keywords=["aws", "ssm", "parameter-store"],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
